2a7ecc77a8fccca3476335f6ca03f4cf4bac11a2
Martin Gonzalez <93826718+PaPitaPaloro@users.noreply.github.com>
2022-05-24 21:24:29 +0200

    se agrega el problema de conversion de moneda

--- script/main.js
+++ script/main.js
@@ -1,103 +1,115 @@
-class Comprador {
-    constructor(nombre,divisa,cantidad){
-        this.nombre = nombre,
-        this.divisa = divisa,
-        this.cantidad = cantidad
-    }
-}
+
 
 
 const dropListFrom = document.querySelector(".drop-list-from select");
 const dropListTo = document.querySelector(".drop-list-to .currency-to select");
 getButton = document.getElementById("get-rate-button");
 
-
+let nombre = "";
 
 //listener del click
 
-getButton.addEventListener("click", e=>{
 
-    e.preventDefault();
-    getExchangeRate();
+
+     getButton.addEventListener("click", m=>{
+
+        
+
+        m.preventDefault();
+        getExchangeRate();
+        
+     } );
     
-});
+
+
+
 
 function getExchangeRate(){
+
     const amount = document.querySelector(".amount input");  //busco el valor que ingresa el usuario para convertir
-    
-
     let amountValue = amount.value;
-    let convertedAmountValue = amountValue;
+        let convertedAmountValue = amountValue;
 
-    if(amountValue == "" || amountValue == "0"){
-        amount.value = "1";
-        amountValue = 1;
 
-    }
+        
+    
+        if(amountValue == "" || amountValue == "0"){
+            amount.value = "1";
+            amountValue = 1;
+    
+        }
+    
+        let currencyFrom = dropListFrom.value;
+        let currencyTo = dropListTo.value;
+    
+        const divisa = ['ARS','USD','EUR'];
+    
+        //TO DO: Consumir una API para romar los valores actualizados de las divisas y modular la funcion de getExchangeRate()
 
-    let currencyFrom = dropListFrom.value;
-    let currencyTo = dropListTo.value;
-
-    const divisa = ['ARS','USD','EUR'];
-
-    //TO DO: Consumir una API para romar los valores actualizados de las divisas y modular la funcion de getExchangeRate()
-    switch (currencyFrom) {
-        case divisa[0]:
-            switch (currencyTo) {
-                case 'USD':
-                    convertedAmountValue = amountValue / 225;               //estos de momento son valores fijos desactualizados, la idea es que se actualicen cuando pueda consumir una api
-                    
-                    break;
-                case 'EUR':
-                    convertedAmountValue = amountValue / 245;
-                    
-                    break;
-                case 'ARS':
-                    
-                    break;
+        switch (currencyFrom) {
+            case divisa[0]:
+                switch (currencyTo) {
+                    case 'USD':
+                        convertedAmountValue = amountValue / 225;               //estos de momento son valores fijos desactualizados, la idea es que se actualicen cuando pueda consumir una api
+                        
+                        break;
+                    case 'EUR':
+                        convertedAmountValue = amountValue / 245;
+                        
+                        break;
+                    case 'ARS':
+                        
+                        break;
+                }
+                
+                break;
+        
+                case divisa[1]:
+                switch (currencyTo) {
+                    case 'ARS':
+                        convertedAmountValue = amountValue * 225;
+                        
+                        break;
+                    case 'EUR':
+                        convertedAmountValue = amountValue * 0.9;
+                        
+                        break;
+                    case 'USD':
+                        
+                        break;
+                }
+                break;
+    
+                case divisa[2]:
+                switch (currencyTo) {
+                    case 'ARS':
+                        convertedAmountValue = amountValue * 245;
+                        
+                        break;
+                    case 'USD':
+                        convertedAmountValue = amountValue * 1.1;
+                        
+                        break;
+                    case 'EUR':
+                        
+                        break;
+                }
+                break;
+    
+            default:
+                break;
             }
             
-            break;
+
+
+        document.getElementById('outputValue').innerHTML = convertedAmountValue.toFixed(2);
     
-            case divisa[1]:
-            switch (currencyTo) {
-                case 'ARS':
-                    convertedAmountValue = amountValue * 225;
-                    
-                    break;
-                case 'EUR':
-                    convertedAmountValue = amountValue * 0.9;
-                    
-                    break;
-                case 'USD':
-                    
-                    break;
-            }
-            break;
 
-            case divisa[2]:
-            switch (currencyTo) {
-                case 'ARS':
-                    convertedAmountValue = amountValue * 245;
-                    
-                    break;
-                case 'USD':
-                    convertedAmountValue = amountValue * 1.1;
-                    
-                    break;
-                case 'EUR':
-                    
-                    break;
-            }
-            break;
+  
 
-        default:
-            break;
-    }
 
-    //convertedAmountValue = Math.floor(convertedAmountValue);
 
-    document.getElementById('outputValue').innerHTML = convertedAmountValue.toFixed(2);
+   
 
 }
 
